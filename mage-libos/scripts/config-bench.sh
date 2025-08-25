312e40155978cf9a3f412e13e19d173c8161bfc3
PanJason <pyyjason@gmail.com>
2025-08-25 14:22:15 +0200

    Fix config

--- mage-libos/scripts/config-bench.sh
+++ mage-libos/scripts/config-bench.sh
@@ -21,7 +21,7 @@ FULL_CLEAN=y
 FULL_MB=60000
 
 function recover() {
-    sed -i "s/constexpr size_t max_tokens = 16;/constexpr size_t max_tokens = 64;/g" ${ROOT_PATH}/dilos/core-ddc/tlb.hh
+    sed -i "s/constexpr size_t max_tokens = 8;/constexpr size_t max_tokens = 64;/g" ${ROOT_PATH}/dilos/core-ddc/tlb.hh
     sed -i "s/constexpr size_t current_max_evict = [0-9]\+;/constexpr size_t current_max_evict = 256;/g" ${ROOT_PATH}/dilos/include/ddc/remote.hh
     sed -i "s/constexpr size_t max_queues = [0-9]\+;/constexpr size_t max_queues = 4;/g" ${ROOT_PATH}/dilos/include/ddc/page.hh
     sed -i "s/static constexpr size_t max = [0-9]\+;/static constexpr size_t max = 512;/g" ${ROOT_PATH}/dilos/core/mempool.cc
